2d9ebad0e20e260665a58b9c14be11834512630b
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-15 23:46:03 +0600

    animation: Achievement

--- src/segments/Achievement/index.jsx
+++ src/segments/Achievement/index.jsx
@@ -1,26 +1,54 @@
 import Odometer from "react-odometerjs";
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 import "odometer/themes/odometer-theme-default.css";
+
 import "./Achievement.css";
 
 const Achievement = () => {
+  const containerRef = useRef(null);
+
   const [clients, setClients] = useState(0);
   const [satisfaction, setSatisfaction] = useState(0);
   const [projects, setProjects] = useState(0);
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setClients(200);
-      setSatisfaction(98);
-      setProjects(500);
-    }, 2000);
+  useGSAP(
+    () => {
+      gsap
+        .timeline({
+          delay: 0.5,
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: "20% bottom",
+            end: "bottom top",
 
-    return () => clearTimeout(timeout);
-  });
+            onEnter: () => {
+              setTimeout(() => {
+                setClients(200);
+                setSatisfaction(98);
+                setProjects(500);
+              }, 2000);
+            },
+          },
+        })
+
+        .fromTo(
+          "#achievement .achievement",
+          { y: 50, opacity: 0 },
+          { y: 0, opacity: 1, stagger: 0.5 }
+        );
+    },
+    { scope: containerRef }
+  );
 
   return (
-    <section id="achievement">
+    <section id="achievement" ref={containerRef}>
       <div className="container">
         <div className="achievement">
           <p className="sub-title">Trusted By</p>
