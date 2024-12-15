c1dcad22295385821e1a0d03c2e8834238bfd851
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-12-16 00:33:02 +0600

    animation: Portfolio

--- src/segments/Portfolio/index.jsx
+++ src/segments/Portfolio/index.jsx
@@ -2,10 +2,45 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import { portfolio } from "../../data";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+import { useRef } from "react";
+
 import "./Portfolio.css";
 const Portfolio = () => {
+  const containerRef = useRef(null);
+
+  useGSAP(
+    () => {
+      gsap
+        .timeline({
+          delay: 0.5,
+          scrollTrigger: {
+            trigger: containerRef.current,
+            start: "20% bottom",
+            end: "bottom top",
+          },
+        })
+
+        .fromTo(
+          [
+            "#portfolio .section-header h3",
+            "#portfolio .section-header h2",
+            "#portfolio .portfolio",
+          ],
+          { y: 50, opacity: 0 },
+          { y: 0, opacity: 1, stagger: 0.5 }
+        );
+    },
+    { scope: containerRef }
+  );
+
   return (
-    <section id="portfolio">
+    <section id="portfolio" ref={containerRef}>
       <div className="container">
         <div className="section-header">
           <h3>Our Portfolio</h3>
